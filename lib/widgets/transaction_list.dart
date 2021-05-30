831e34fe304d0f7c7ec6038faaf51c5c23cc4308
Rokibul Hasan Rijon <44047172+Rijon1810@users.noreply.github.com>
2021-05-30 16:47:27 +0600

    30/05/2021

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,74 @@
+import 'package:expense_tracker/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 450,
+      child: transactions.isEmpty
+          ? LayoutBuilder(builder: (ctx, constrains) {
+              return Column(
+                children: [
+                  Text('No Transactions added yet!',
+                      style: Theme.of(context).textTheme.title),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: constrains.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            })
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                              '\৳${transactions[index].amount.toStringAsFixed(2)}'),
+                        ),
+                      ),
+                    ),
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.title),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () {
+                        deleteTx(transactions[index].id);
+                      },
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
+  }
+}
